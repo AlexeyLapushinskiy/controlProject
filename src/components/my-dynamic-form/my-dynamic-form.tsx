1e3c0f09d39ea0badb8aac1e521dc4ed4c696004
altrofimov <a.d.trofimov@gmail.com>
2018-01-22 22:29:38 +0100

    moving business logic from submit button to form

--- src/components/my-dynamic-form/my-dynamic-form.tsx
+++ src/components/my-dynamic-form/my-dynamic-form.tsx
@@ -1,37 +1,29 @@
-import { Element, Component, Prop, State, Listen } from '@stencil/core';
+import {Element, Component, Prop, State, Listen} from '@stencil/core';
 import * as ajv from 'ajv/dist/ajv.min.js';
 
 @Component({
-	tag: 'my-dynamic-form',
-	shadow: true
+  tag: 'my-dynamic-form',
+  shadow: true
 })
 export class MyDynamicForm {
-
   @State() allTitles: any = {};
+  @State() invalidMessage: string = null;
+
+  @Element() el: HTMLElement;
 
   /**
    * @Prop {any} schema - JSON-schema
    * @Prop {any} form - form for JSON-schema
    */
-
-	@Element() el: HTMLElement;
-	@Prop() schema: any;
-	@Prop() form: any;
-
-  /**
-   * @State {string} invalidMessage - message about invalid data
-   */
-  @State() invalidMessage: string = null;
+  @Prop() schema: any;
+  @Prop() form: any;
 
   @Listen('validateForm')
   validateFormHandler() {
     this.validateForm();
   }
 
-  /**
-   * Object for all properties of the JSON schema
-   */
-	mapping: Object = {};
+  mapping: Object = {}; // properties of the JSON schema
   validate;
 
   /**
@@ -40,89 +32,94 @@ export class MyDynamicForm {
   validateForm() {
     this.validate = ajv.compile(this.schema);
 
-    let myDinamicForm: any  = document.querySelector('my-dynamic-form');
-
-    let data = {
-      "checked": this.getCheckboxValue(myDinamicForm),
-      "duration": this.getDurationValue(myDinamicForm),
-      "startDate": this.getStartDateValue(myDinamicForm),
-      "endDate": this.getEndDateValue(myDinamicForm),
-      "sources": this.getSourcesValue(myDinamicForm)
+    let formData = {
+      "checked": this.getCheckboxValue(),
+      "duration": this.getDurationValue(),
+      "startDate": this.getStartDateValue(),
+      "endDate": this.getEndDateValue(),
+      "sources": this.getSourcesValue()
     };
 
-    /**
-     * Check validation and call function for change message if data is invalid
-     */
-    let valid = this.validate(data);
-    if (valid) {
+    if (this.validate(formData)) {
       this.invalidMessage = null;
     } else {
-      this.invalidMessage = this.editMessage();
+      this.invalidMessage = this.updateValidationMessage();
     }
   };
 
-  getCheckboxValue (myDinamicForm) {
-    let myCheckbox: any  = myDinamicForm.shadowRoot.querySelector('my-checkbox');
+  getCheckboxValue() {
+    let myCheckbox: any = this.el.shadowRoot.querySelector('my-checkbox');
     let checkbox: any = myCheckbox.shadowRoot.getElementById('check-schema');
-    let checkboxValue = checkbox.getAttribute("checked") === "true" ? true : false;
-    return checkboxValue;
+
+    return checkbox.getAttribute("checked") === "true";
   };
 
-  getStartDateValue(myDinamicForm) {
-    let myDates: any  = myDinamicForm.shadowRoot.querySelectorAll('my-text-input');
+  getStartDateValue() {
+    let myDates: any = this.el.shadowRoot.querySelectorAll('my-text-input');
     let startDateValue: string = null;
+
     for (let el of myDates) {
       let shadowEl: any = el.shadowRoot.querySelector('label');
       let elText: string = shadowEl.innerText;
       let elValue: string = el.shadowRoot.querySelector('input').value;
+
       if (elText.trim() === "The Startdate Schema") {
         startDateValue = elValue;
       }
     }
+
     return startDateValue;
   };
 
-  getEndDateValue(myDinamicForm) {
-    let myDates: any  = myDinamicForm.shadowRoot.querySelectorAll('my-text-input');
+  getEndDateValue() {
+    let myDates: any = this.el.shadowRoot.querySelectorAll('my-text-input');
     let endDateValue: string = null;
+
     for (let el of myDates) {
       let shadowEl: any = el.shadowRoot.querySelector('label');
       let elText: string = shadowEl.innerText;
       let elValue: string = el.shadowRoot.querySelector('input').value;
+
       if (elText.trim() === "The Enddate Schema") {
         endDateValue = elValue;
       }
     }
+
     return endDateValue;
   };
 
-  getDurationValue (myDinamicForm) {
-    let myNumberInput: any  = myDinamicForm.shadowRoot.querySelectorAll('my-number-input');
+  getDurationValue() {
+    let myNumberInput: any = this.el.shadowRoot.querySelectorAll('my-number-input');
     let durationValue: any = {};
     let el: HTMLElement = null;
-    for(el of myNumberInput) {
-      let labelText: string  = el.shadowRoot.querySelector('label').innerText;
-      let inputValue: string  = el.shadowRoot.querySelector('input').value;
+
+    for (el of myNumberInput) {
+      let labelText: string = el.shadowRoot.querySelector('label').innerText;
+      let inputValue: string = el.shadowRoot.querySelector('input').value;
+
       labelText.trim() === "The Min Schema" ? durationValue.min = parseInt(inputValue) : durationValue.max = parseInt(inputValue);
     }
+
     return durationValue;
   };
 
-  getSourcesValue(myDinamicForm) {
-    let myTextInputArray: any  = myDinamicForm.shadowRoot.querySelector('my-text-input-array');
+  getSourcesValue() {
+    let myTextInputArray: any = this.el.shadowRoot.querySelector('my-text-input-array');
     let dropdown: any = myTextInputArray.shadowRoot.querySelector('my-dropdown');
     let select: any = dropdown.shadowRoot.querySelector('select');
     let options: any = select.querySelectorAll('option');
     let sourcesValue: any = [];
     let el: HTMLElement = null;
-    for(el of options) {
+
+    for (el of options) {
       let optionText: string = el.innerText;
       sourcesValue.push(optionText);
     }
+
     return sourcesValue;
   };
 
-  editMessage() {
+  updateValidationMessage() {
     let unchangedMessage: any = ajv.errorsText(this.validate.errors).replace(/\,?\w*\.\w*\./g, "").split(" ");
     Object.keys(this.allTitles).map((title: string) => {
       for (let el in unchangedMessage) {
@@ -130,50 +127,56 @@ export class MyDynamicForm {
           unchangedMessage[el] = this.allTitles[title];
         }
       }
-    })
-    let changedMessage: string = unchangedMessage.toString().replace(/\,(?!\,)/g, " ");
-    return changedMessage;
+    });
+
+    return unchangedMessage.toString().replace(/\,(?!\,)/g, " ");
   };
 
   /**
-   * Function for getting fields based on properties of properties in JSON-schema.
+   * Getting fields based on properties of properties in JSON schema
    */
-
   getFirstProps(propsKeyProps, key, keyProp) {
-    let { type } = propsKeyProps[keyProp];
+    let {type} = propsKeyProps[keyProp];
     let Tag = this.mapping[type];
     let title = propsKeyProps[keyProp].title;
     this.allTitles[keyProp] = title;
-    return this.form[key].hasOwnProperty(keyProp) ? <Tag for={keyProp} value={JSON.stringify(this.form[key][keyProp])} title={title} /> : null;
+
+    return this.form[key].hasOwnProperty(keyProp) ?
+      <Tag for={keyProp} value={JSON.stringify(this.form[key][keyProp])} title={title}/> : null;
   };
 
   /**
-   * Function for getting fields based on properties in JSON-schema.
+   * Getting fields based on properties in JSON-schema
    */
-
   getSecondProps(props, key) {
-    let { type } = props[key];
+    let {type} = props[key];
     let Tag = this.mapping[type];
     let title: string = props[key].title;
     this.allTitles[key] = title;
-    if(!title) {
+
+    if (!title) {
       props[key].items ? title = props[key].items.title : title = 'Unnamed field';
       this.allTitles[key] = title;
     }
+
     if (key === "button") {
-      return this.form.hasOwnProperty(key) ? <Tag for={key} value={JSON.stringify(this.form[key])} title={title} allTitles={this.allTitles} /> : null;
+      return this.form.hasOwnProperty(key) ?
+        <Tag for={key} value={JSON.stringify(this.form[key])} title={title} allTitles={this.allTitles}/> : null;
     }
-    return this.form.hasOwnProperty(key) ? <Tag for={key} value={JSON.stringify(this.form[key])} title={title} /> : null;
+
+    return this.form.hasOwnProperty(key) ? <Tag for={key} value={JSON.stringify(this.form[key])} title={title}/> : null;
   };
 
-	render() {
+  render() {
     /**
-     * Creating form fields and saving it to the let form.
+     * Creating form fields and saving it to the let form
      */
-	  let form: any = Object.keys(this.schema.properties).map((key: any) => {
+    let message: any = null;
+
+    let form: any = Object.keys(this.schema.properties).map((key: any) => {
       let props: any = this.schema.properties;
-      if(props[key].properties) {
-        return Object.keys(props[key].properties).map ((keyProp: any) => {
+      if (props[key].properties) {
+        return Object.keys(props[key].properties).map((keyProp: any) => {
           let propsKeyProps: any = props[key].properties;
           return this.getFirstProps(propsKeyProps, key, keyProp);
         })
@@ -182,8 +185,6 @@ export class MyDynamicForm {
       }
     });
 
-    let message: any = null;
-
     if (this.invalidMessage) {
       message =
         <div>
@@ -191,20 +192,20 @@ export class MyDynamicForm {
         </div>;
     }
 
-		return (
-        <div>
-          {form}
+    return (
+      <div>
+        {form}
 
-          {message} <br />
-        </div>
-		);
-	}
+        {message} <br/>
+      </div>
+    );
+  }
 
-	componentWillLoad() {
-		for (var i = 0; i < this.el.children.length; i++) {
-			let child = this.el.children[i];
-			let mapKey = child['for'];
+  componentWillLoad() {
+    for (let i = 0; i < this.el.children.length; i++) {
+      let child = this.el.children[i];
+      let mapKey = child['for'];
       this.mapping[mapKey] = child['localName'];
-		}
-	}
+    }
+  }
 }
